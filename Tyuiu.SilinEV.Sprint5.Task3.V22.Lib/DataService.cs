cac00026820005771647892ad4d1db96ecde494c
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-27 22:22:25 +0500

    Task3

---
+++ Tyuiu.SilinEV.Sprint5.Task3.V22.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SilinEV.Sprint5.Task3.V22.Lib
+{
+    public class DataService : ISprint5Task3V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+            return Convert.ToString(Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 3));
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint5.Task3.V22.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint5.Task3.V22.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
