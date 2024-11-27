17bc2ab2932db3652ffbfba9af25d79f187392bc
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-27 21:22:45 +0500

    Task1

---
+++ Tyuiu.SilinEV.Sprint5.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SilinEV.Sprint5.Task1.V17.Lib
+{
+    public class DataService : ISprint5Task1V17
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string a = "";
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                a += Convert.ToString(2 * i - 4 + ((2 * i - 1) / (Math.Sin(i) + 1))) + Environment.NewLine;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint5.Task1.V17.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint5.Task1.V17.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
