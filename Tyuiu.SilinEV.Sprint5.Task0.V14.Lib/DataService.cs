6af47f8ac51411c02c63db0362c365663804bf95
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-27 20:57:58 +0500

    Add project files.

---
+++ Tyuiu.SilinEV.Sprint5.Task0.V14.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SilinEV.Sprint5.Task0.V14.Lib
+{
+    public class DataService : ISprint5Task0V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            return Convert.ToString(Math.Round((4 * Math.Pow(x, 3)) / (Math.Pow(x, 3) - 1), 3));
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint5.Task0.V14.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint5.Task0.V14.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
