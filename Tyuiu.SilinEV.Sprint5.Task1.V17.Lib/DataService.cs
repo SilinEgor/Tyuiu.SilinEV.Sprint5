a9545ec04d5deacbc7e992aeb67cd3c053ba07a4
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-11-27 21:28:50 +0500

    Task1

--- Tyuiu.SilinEV.Sprint5.Task1.V17.Lib/DataService.cs
+++ Tyuiu.SilinEV.Sprint5.Task1.V17.Lib/DataService.cs
@@ -10,7 +10,8 @@ namespace Tyuiu.SilinEV.Sprint5.Task1.V17.Lib
 
             for (int i = startValue; i <= stopValue; i++)
             {
-                a += Convert.ToString(Math.Round(2 * i - 4 + ((2 * i - 1) / (Math.Sin(i) + 1)), 2)) + Environment.NewLine;
+                a += Convert.ToString(Math.Round(2 * i - 4 + ((2 * i - 1) / (Math.Sin(i) + 1)), 2));
+                if (i != stopValue) a += Environment.NewLine;
             }
 
             return a;
