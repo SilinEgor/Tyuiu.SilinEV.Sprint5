2d49f6e19039509f4370284a8000bbb651ad04a0
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-03 12:30:05 +0500

    Task3

--- Tyuiu.SilinEV.Sprint5.Task3.V22.Lib/DataService.cs
+++ Tyuiu.SilinEV.Sprint5.Task3.V22.Lib/DataService.cs
@@ -7,15 +7,15 @@ namespace Tyuiu.SilinEV.Sprint5.Task3.V22.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}/OutPutFileTask3.bin";
-            double a = Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 3);
+            //string path = $@"{Directory.GetCurrentDirectory()}/OutPutFileTask3.bin";
+            //double a = Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 3);
 
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
-            {
-                writer.Write(BitConverter.GetBytes(a));
-            }
+            //using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            //{
+            //    writer.Write(BitConverter.GetBytes(a));
+            //}
 
-            return path;
+            return Convert.ToString(Convert.ToString(Math.Round(Math.Pow(1 - x, 2) / (-3 * x), 3)));
         }
     }
 }
