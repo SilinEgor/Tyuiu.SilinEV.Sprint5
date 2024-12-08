93373547b57e0c274af27a1f46a4d6f0244a88d6
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-08 16:37:42 +0500

    Task7

---
+++ Tyuiu.SilinEV.Sprint5.Task7.V20.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SilinEV.Sprint5.Task7.V20.Lib
+{
+    public class DataService : ISprint5Task7V20
+    {
+        public string LoadDataAndSave(string path)
+        {
+            return File.ReadAllText(path).Replace("сс", "с");
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint5.Task7.V20.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint5.Task7.V20.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
