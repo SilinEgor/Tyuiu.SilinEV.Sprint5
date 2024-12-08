406d44250878808eeb6f83f1d3a53661f1579e71
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-08 16:32:50 +0500

    Task6

---
+++ Tyuiu.SilinEV.Sprint5.Task6.V10.Lib/DataSevice.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SilinEV.Sprint5.Task6.V10.Lib
+{
+    public class DataSevice : ISprint5Task6V10
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string[] a = File.ReadAllText(path).Replace(".", "").Split(", ");
+            int s = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Length == 4) s++;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint5.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint5.Task6.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
