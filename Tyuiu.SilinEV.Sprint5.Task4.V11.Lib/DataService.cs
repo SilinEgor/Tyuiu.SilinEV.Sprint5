655a450d50e2387e5253e596987a4e17716faac9
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-12-08 16:10:14 +0500

    Task4

---
+++ Tyuiu.SilinEV.Sprint5.Task4.V11.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SilinEV.Sprint5.Task4.V11.Lib
+{
+    public class DataService : ISprint5Task4V11
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double a = Convert.ToDouble(File.ReadAllText(path));
+
+            return Math.Round(Math.Sin(a) + (a * a) / 2, 3);
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint5.Task4.V11.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint5.Task4.V11.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
